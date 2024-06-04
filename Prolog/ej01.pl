fc2f27e15cf7954b50b416169949d8320675e3e2
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-06-04 03:41:56 -0300

    continue

--- Prolog/ej01.pl
+++ Prolog/ej01.pl
@@ -4,7 +4,7 @@ progenitor(jorge,carlos). progenitor(tomas,pedro). progenitor(jose,juan).
 progenitor(daniel,laura). progenitor(andrea,laura). progenitor(rodolfo,damian). 
 progenitor(claudia,damian). 
 progenitor(juan,ana). progenitor(juan,luis).
-progenitor(maria, ana).  progenitor(maria,pablo). progenitor(maria,luis).
+progenitor(maria,ana).  progenitor(maria,pablo). progenitor(maria,luis).
 progenitor(pedro,pablo). progenitor(pedro,luciano). progenitor(pedro,ariel).
 progenitor(laura,luciano). progenitor(laura,ariel). 
 progenitor(eugenia,maria). progenitor(eugenia,rodolfo). progenitor(eugenia,sandra).
@@ -38,19 +38,100 @@ ancestro(X,Y):- progenitor(X,W),ancestro(W,Y).
 
 /* b) Considere agregar los siguientes hechos: */
 casado(juan, maria).
+casado(pedro, laura).
 casado(eugenia, martin).
 casado(daniel, andrea).
 casado(cecilia,javier).
-casado(pedro, laura).
 casado(rodolfo, claudia).
 casado(norma,carlos).
 
+% defino matrimonio para utilizar casado(X,Y) en ambos sentidos
+matrimonio(X,Y):- casado(X,Y).
+matrimonio(X,Y):- casado(Y,X).
+
 /* c) Defina e implemente en PROLOG los predicados: */
 % soltero
-soltero(X):- not(casado(X,_)).
+soltero(X):- \+matrimonio(X,_).
 % amante
-amante(X,Y):- soltero(X), casado(Y,_).
-amante(X,Y):- soltero(Y), casado(X,_).
+amante(X,Y):- matrimonio(X,M),progenitor(X,H),progenitor(Y,H),M\=Y,X\=Y.
+amante(X,Y):- matrimonio(Y,M),progenitor(Y,H),progenitor(Y,H),M\=X,X\=Y.
 % suegra y suegro
-suegra(X,Y):- casado(X,W), madre(Y,W).
-suegro(X,Y):- casado(X,W), padre(Y,W),
+suegra(X,Y):- matrimonio(Y,W), madre(X,W).
+suegro(X,Y):- matrimonio(Y,W), padre(X,W).
+
+/* d) Indique la/s respuesta/s de las siguientes consultas:
+?- madre(X,laura).
+X = andrea.
+?- abuelo(tomas,Y)
+Y = pablo ;
+Y = luciano ;
+Y = ariel.
+?- ancestro(X,luciano).
+X = pedro ;
+X = laura ;
+X = silvia ;
+X = jorge ;
+X = tomas ;
+X = daniel ;
+X = andrea ;
+X = norma ;
+X = carlos ;
+false.
+?- ancestro(jorge,Y).
+Y = carlos ;
+Y = andrea ;
+Y = cecilia ;
+Y = laura ;
+Y = luciano ;
+Y = ariel ;
+Y = gabriel ;
+false.
+?- soltero(luis).
+true.
+?- soltero(jose): ¿es correcta la respuesta?
+true.
+?- amante(X,Y). 
+false. (deberia retornar otra cosa?) */
+
+/* e) Realice las siguientes consultas e indique la respuesta:
+Según su definición ¿es Pablo hermano de Luis?
+?- hermano(pablo,luis).
+false.
+Pedro, ¿de quién es padre?
+?- padre(pedro,X).
+X = pablo ;
+X = luciano ;
+X = ariel.
+¿Quién es suegro de María?
+?- suegro(X,maria). 
+X = jose.
+¿Quiénes son hermanos?
+?- hermano(X,Y). 
+X = ana, Y = luis ;
+X = luis, Y = ana ;
+X = luciano, Y = ariel ;
+X = ariel, Y = luciano ;
+X = maria, Y = rodolfo ;
+X = maria, Y = sandra ;
+X = rodolfo, Y = maria ;
+X = rodolfo, Y = sandra ;
+X = sandra, Y = maria ;
+X = sandra, Y = rodolfo ;
+X = andrea, Y = cecilia ;
+X = cecilia, Y = andrea ;
+false.
+¿De quién es primo Damián?
+?- primo(damian,X).
+X = ana ;
+X = pablo ;
+X = luis ;
+false.
+¿Quiénes son tíos de Pablo?
+
+¿De quién es Luciano bisnieto?
+
+¿Quiénes son descendientes de Silvia?
+
+¿Quiénes son solteros?
+
+*/
