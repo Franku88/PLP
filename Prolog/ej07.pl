c72bf062849f2ff2ecb601b15048b0ebf84f1a27
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-06-13 21:15:49 -0300

    continue

---
+++ Prolog/ej07.pl
@@ -0,0 +1,9 @@
+/* 7. Escribir programas para implementar las siguientes operaciones sobre listas:
+a) concatenación
+b) pertenencia de un elemento.
+c) cantidad de ocurrencias de un elemento.
+d) eliminación de la primera ocurrencia de un elemento.
+e) sustitución de la primera ocurrencia de un elemento por otro.
+f) longitud. Por ejemplo entra [1,2,1,3,4] y sale 5.
+g) prefijo
+*/
