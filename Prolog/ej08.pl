fbfac1e3491e7504a1571a511a1720ee0cd96f1b
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-06-14 22:39:20 -0300

    continue

---
+++ Prolog/ej08.pl
@@ -0,0 +1,21 @@
+/*8. Escribir programas para implementar las siguientes operaciones sobre listas:*/
+%a) rotación a la izquierda. Ej. entra [1,2,3,4] y sale [2,3,4,1]
+
+%b) rotación a la derecha. Ej. entra [1,2,3,4] y sale [4,1,2,3]
+
+%c) eliminación de todas las ocurrencias de un elemento.
+
+%d) sustitución de todas las ocurrencias de un elemento por otro.
+
+%e) cardinalidad (cantidad de elementos no repetidos) Ej. entra [1,2,1,3,4] y sale 4.
+
+%f) inversión. Ej. entra [1,2,3,4] y sale [4,3,2,1].
+
+%g) sufijo
+
+/*h) generar a partir de una lista el palíndrome correpondiente. Por ejemplo, a partir de
+abcd se genera el palíndromo abcddcba.
+*/
+
+/*i) duplicar los elementos de una lista. Por ejemplo, el resultado de doblar [1,2,3] es
+[1,1,2,2,3,3].*/
